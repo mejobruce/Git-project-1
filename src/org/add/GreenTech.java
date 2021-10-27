0b91eb183f8a4dff9e60a71ccfd551b8ddd40642
mejobruce <mejobruce@gmail.com>
2021-10-27 15:07:56 +0530

    done

---
+++ src/org/add/GreenTech.java
@@ -0,0 +1,26 @@
+package org.add;
+
+public class GreenTech {
+	private void greensOmr(int order) {
+		// TODO Auto-generated method stub
+System.out.println("GreenTech:" + order);
+	}
+	private void greensOmr(int number,String type) {
+		// TODO Auto-generated method stub
+System.out.println("GreenTech  No:" + number + "\tGreenTech:" + type);
+	}
+	private void greensOmr(String type,int number) {
+		// TODO Auto-generated method stub
+System.out.println("GreenTech No:" + number + "\tGreenTech:" + type);
+	}
+	
+public static void main(String[] args) {
+	GreenTech g = new GreenTech();
+	g.greensOmr(001);
+	g.greensOmr(988654876, "java class");
+	g.greensOmr("pythen class", 6567865);
+	
+	
+
+}
+}
