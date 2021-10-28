cc2fe0fb44f48526cae7adc0ab3eb8cd443b19de
mejobruce <mejobruce@gmail.com>
2021-10-28 12:11:44 +0530

    sdfghjkl

--- src/org/add/GreenTech.java
+++ src/org/add/GreenTech.java
@@ -37,9 +37,20 @@ public static void main(String[] args) {
 	System.out.println(123);
 	System.out.println(123);
 	System.out.println(123);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
+	System.out.println(456);
 	System.out.println(123);
-	
-	
-
+	System.out.println(123);
+	System.out.println(123);
+	System.out.println(123);
+	System.out.println(123);
 }
 }
