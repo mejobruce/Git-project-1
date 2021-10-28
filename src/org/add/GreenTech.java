4e84751b31fe736d49ca1e11f4836606852470c0
mejobruce <mejobruce@gmail.com>
2021-10-28 12:16:23 +0530

    asdfghjkl

--- src/org/add/GreenTech.java
+++ src/org/add/GreenTech.java
@@ -38,6 +38,46 @@ public static void main(String[] args) {
 	System.out.println(123);
 	System.out.println(123);
 	System.out.println(123);
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
+	System.out.println("abc");
 	
 	
 
